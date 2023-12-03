243883c757250d13388b1da9550d1f92c7144071
Tove <tovevc@kth.se>
2023-12-03 19:23:52 +0100

    l3 p1 working

--- 2023/l3.py
+++ 2023/l3.py
@@ -1,4 +1,4 @@
-non_symbols = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+non_symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
 
 
 def is_symbol(char):
@@ -22,7 +22,7 @@ def adjacent_indices(index, no_of_rows, no_of_columns):
     if i != 0 and j != 0:
         adjacent_indices.add((i - 1, j - 1))
     if i != 0:
-        adjacent_indices.add((1 - i, j))
+        adjacent_indices.add((i - 1, j))
     if j != 0:
         adjacent_indices.add((i, j - 1))
     if i != no_of_rows - 1 and j != no_of_columns - 1:
@@ -42,7 +42,7 @@ print(adjacent_indices((5, 7), 6, 8))
 
 
 def read_file():
-    with open("input3.txt", 'r') as inp:
+    with open("input3.txt", 'r', encoding="UTF-8") as inp:
         return inp.readlines()
 
 
@@ -53,27 +53,29 @@ def is_adjacent(index, matrix):
             or is_symbol(matrix[i + 1][j - 1]) or is_symbol(matrix[i + 1][j]) or is_symbol(matrix[i + 1][j + 1]))
 
 
-def find_number_in_line(line, start_at):
+def find_number_in_line(line, line_nr, start_at):
     number = '0'
     number_indices = []
     for c in range(start_at, len(line)):
         if (line[c].isdigit()):
             number += line[c]
-            number_indices.append((0, c))
-            if c+1 == len(line) - 1 or not line[c + 1].isdigit():
+            number_indices.append((line_nr, c))
+            if c == len(line) - 1 or not line[c + 1].isdigit():
                 break
-    return int(number), number_indices, c+1
+    return int(number), number_indices, c + 1
 
-def find_all_numbers_in_line(line):
+
+def find_all_numbers_in_line(line, line_nr):
     numbers_on_line = []
     numbers_on_line_indices = []
     start_at = 0
-    while start_at <= len(line)-1:
-        found_number, found_number_indices, start_at = find_number_in_line(line, start_at)
+    while start_at <= len(line) - 1:
+        found_number, found_number_indices, start_at = find_number_in_line(line, line_nr, start_at)
         numbers_on_line.append(found_number)
         numbers_on_line_indices.append(found_number_indices)
 
     return numbers_on_line, numbers_on_line_indices
+
 
 def is_part_number(number, no_of_lines, line_len, symbol_indices):
     value, indices = number
@@ -90,26 +92,27 @@ def main():
     data = [s.strip() for s in read_file()]
     no_of_lines = len(data)  # aka number of rows
     line_len = len(data[0])  # aka number of columns
-    print(data)
+    # print(data)
 
     # make a char by char matrix
     elem_matrix = []
     for line in data:
         elem_matrix.append([ch for ch in line])
-    print(elem_matrix)
+    # print(elem_matrix)
 
     symbol_indices = find_symbols(elem_matrix)
 
     # do the thing
+    line_nr = 0
     for line in data:
-        numbers_on_line, numbers_on_line_indices = find_all_numbers_in_line(line)
+        numbers_on_line, numbers_on_line_indices = find_all_numbers_in_line(line, line_nr)
         number_candidates = zip(numbers_on_line, numbers_on_line_indices)
         for number_candidate in number_candidates:
-            print(number_candidate)
             if is_part_number(number_candidate, no_of_lines, line_len, symbol_indices):
                 sum_of_part_numbers += number_candidate[0]
+        line_nr += 1
 
-    return  sum_of_part_numbers
+    return sum_of_part_numbers
 
 
 if __name__ == "__main__":
