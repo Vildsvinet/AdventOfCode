c0346588d5dab082d37f546186a8107440bb3e09
Tove <tovevc@kth.se>
2023-12-05 21:09:08 +0100

    l5 p1 working

--- 2023/l5.py
+++ 2023/l5.py
@@ -1,4 +1,3 @@
-
 def read_input(filename):
     seeds = []
     maps = []
@@ -9,16 +8,18 @@ def read_input(filename):
 
         for line in inp:
             line = line.strip()
+            # when empty line, close and add new map to maps
             if line == "" and current_map:
                 maps.append(current_map)
-                current_map.clear()
+                current_map = []
                 current_map.append(inp.readline())
             elif line == "" and not current_map:
-                continue
+                current_map.append(inp.readline().strip())
             else:
-                current_map.append(parse_input(line))
+                current_map.append([int(s) for s in line.split(' ')])
+        maps.append(current_map)
 
-    return seeds
+    return seeds, maps
 
 
 def parse_seeds(seed_line: str):  # -> list:
@@ -26,31 +27,38 @@ def parse_seeds(seed_line: str):  # -> list:
     return [int(s) for s in str_seeds]
 
 
-def parse_input(line):
-    print(line.split(' '))
-    # destination_range_start, source_range_start, range_length = line.split(' ')
-    # print(destination_range_start, source_range_start, range_length)
-    pass
-
-print(read_input("input5.txt"))
-
-# when empty line, close and add new element to maps
-# a, b, c = maps (unpack them again)
-
-# if nr in range(source_range_start, range_length):
-#   follow nr
-
 def find_destination_from_source(nr: int, amap) -> int:
     """
-    See what
-    :param nr:
-    :param amap:
+    See what destination a source has
+    :param nr: source number
+    :param amap: the current map, eg seedtosoil or watertolight
     :return:
     """
-    next = 0
-    return next
+    dest = nr  # default: maps to itself if not stated otherwise
+    for line in amap[1:]:
+
+        destination_range_start, source_range_start, range_length = line
+        if nr in range(source_range_start, source_range_start + range_length):
+            offset = nr - source_range_start
+            dest = destination_range_start + offset
+            break
+
+    return dest
 
 
-def find_location_from_seed(seed: int):
-    location = 0
-    return location
+def find_location_from_seed(seed: int, maps: list):
+    source = seed
+    for i in range(len(maps)):  # this destination is next source
+        source = find_destination_from_source(source, maps[i])
+
+    destination = source
+    return destination
+
+
+if __name__ == '__main__':
+    seeds, maps = read_input("input5.txt")
+    # print(find_destination_from_source(55, maps[0]))
+    seed_locations = []
+    for seed in seeds:
+        seed_locations.append(find_location_from_seed(seed, maps))
+    print(min(seed_locations))
