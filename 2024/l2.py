d661033803c62b1f0ab96254697c0e016d239bd1
Tove <tovevc@kth.se>
2024-12-02 20:34:10 +0100

    2024 day2 part1

---
+++ 2024/l2.py
@@ -0,0 +1,18 @@
+# Advent of Code, 2024, day 2
+def is_safe(rpt: list) -> bool:
+    asc = -1 # -1=not set; 1=asc 0=desc
+    for i in range(len(rpt)-1):
+        d = rpt[i] - rpt[i+1]
+        if abs(d) > 3 or d == 0 or (asc == 1 and d > 0) or (asc == 0 and d < 0):
+            return False
+
+        asc = 1 if d < 0 else 0
+
+    return True
+
+with open("i2.txt", 'r') as inp:
+    safe_reports = 0
+    for report in inp:
+        if is_safe(list(map(int,report.split()))): safe_reports += 1
+
+    print(safe_reports)
