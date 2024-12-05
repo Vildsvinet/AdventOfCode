73fed22bdba928e7c69637533fd5e030d92fdc5e
Tove <tovevc@kth.se>
2024-12-05 22:50:38 +0100

    2024 day4 part2

--- 2024/l4.py
+++ 2024/l4.py
@@ -1,6 +1,7 @@
 # Advent of Code, 2024, day 4
 import re
 
+
 def left_to_right(st: str) -> int:
     return len(re.findall('XMAS', st))
 
@@ -9,15 +10,13 @@ def right_to_left(st: str) -> int:
     return left_to_right(st[::-1])
 
 
-def construct_columns(longtext) -> list:
+def construct_columns(longtext: str) -> list:
     no_of_columns = longtext.index('\n') + 1
     columns = ['' for _ in range(no_of_columns)]
-    # print(no_of_columns, len(columns))
+
     for i in range(no_of_columns):
         for c in longtext[i::no_of_columns]:
             columns[i] += c
-
-    # print(columns)
     return columns
 
 
@@ -30,10 +29,8 @@ def vertical(longtext: str) -> int:
     return counter
 
 
-def construct_diags(longtext: str) -> list:
-    rows : list[str] = content.split('\n')
+def construct_diags(rows: list[str]) -> list:
     no_of_columns = len(rows[0])
-
     no_of_diags = 2 * no_of_columns - 1
 
     diags = ['' for _ in range(no_of_diags)]
@@ -44,33 +41,52 @@ def construct_diags(longtext: str) -> list:
         for charnr, char in enumerate(row[::-1]):
             diags[charnr + rownr] += char
 
-    # anti diagonals
+    # antidiagonals
     for rownr, row in enumerate(rows):
         for charnr, char in enumerate(row):
             anti_diags[charnr + rownr] += char
-    # print(diags+anti_diags)
 
     return diags + anti_diags
 
 
-def diagonal(longtext: str):
+def diagonal(rows: list[str]):
     counter = 0
-    diags = construct_diags(longtext)
+    diags = construct_diags(rows)
     for d in diags:
         counter += left_to_right(d)
         counter += right_to_left(d)
     return counter
 
+# part 2
+def x_mas(fullcontent):
+    tot_count = 0
+
+    rows = content.split('\n')
+    nr_of_rows = len(rows)
+    row_len = len(rows[0])
+
+    for rownr in range(nr_of_rows - 2):  # range(nr_of_rows - 2)
+        for c in range(row_len - 2):
+            box = rows[rownr][c:c + 3] + rows[rownr + 1][c:c + 3] + rows[rownr + 2][c:c + 3]
+            if (re.search("M.M.A.S.S|M.S.A.M.S|S.M.A.S.M|S.S.A.M.M", box)):
+                tot_count += 1
+
+    return tot_count
+
 
 with open("i4.txt", 'r') as inp:
     content = inp.read()
+
+    # part 1
     tot = 0
-    #left to right
-    ltr_content_list = content.split('\n')
-    for line in ltr_content_list:
+    content_list = content.split('\n')
+    for line in content_list:
         tot += left_to_right(line)
         tot += right_to_left(line)
 
     tot += vertical(content)
-    tot += diagonal(content)
+    tot += diagonal(content_list)
     print(tot)
+
+    # part 2
+    print(x_mas(content))
