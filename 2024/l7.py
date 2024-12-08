c52105b868e1d33fa6ceb2ae0f22f609fcd8f975
Tove <tovevc@kth.se>
2024-12-08 01:46:09 +0100

    2024 day7 part2

--- 2024/l7.py
+++ 2024/l7.py
@@ -2,7 +2,7 @@
 
 import sys
 
-sys.setrecursionlimit(2 ** 12 + 1)
+sys.setrecursionlimit(3 ** 12 + 1)
 
 totsum = 0
 
@@ -49,7 +49,90 @@ def run(sofar, result):
         return run(rollback(sofar), result)
 
 
+
+
+################################## PART 2 ######################################
+
+def concat(a, b): return int(str(a) + str(b))
+def plus(a, b): return a + b
+def times(a, b): return a * b
+
+
+class Node:
+    def __init__(self, value, operator):
+        self.value = value
+        self.operator = operator
+
+    def next_operator(self):
+        if self.operator == concat:
+            self.operator = plus
+        elif self.operator == plus:
+            self.operator = times
+        elif self.operator == times:
+            self.operator = None
+
+    def reset_operator(self):
+        self.operator = concat
+
+    def __str__(self):
+        return f"{self.operator} : {self.value}"
+
+
+# only for calculating a specific combination
+def calculate_list(ls):
+    l = ls[:]
+    if len(l) == 2:
+        return l[1].operator(l[0].value, l[1].value)
+    nod0 = l.pop(0)
+    nod1 = l.pop(0)
+    new = Node(nod1.operator(nod0.value, nod1.value), None)
+    return calculate_list([new] + l)
+
+
+# move to the next (right) branch of the tree
+def rollback_list(ls: list[Node]) -> list:
+    l = ls[:]
+    keep = [l.pop(0)]
+
+    for pos, n in reversed(list(enumerate(l))):
+        if n.operator != times:
+            n.next_operator()
+            for m in l[pos + 1:]:
+                m.reset_operator()
+            return keep + l
+    return None
+
+
+# pre: lista is Node objects, full length... quite pointless to do recursion
+def run_recursive(lista, res):
+    if not lista:  # we went to everything and found nothing
+        return False
+    if calculate_list(lista) == res:  # we found a match for the operators
+        return True
+
+    return run_recursive(rollback_list(lista), res)
+
+
+def run_loop(lista, res):
+    while True:
+        if not lista:  # we went to everything and found nothing
+            return False
+        if calculate_list(lista) == res:  # we found a match for the operators
+            return True
+        lista = rollback_list(lista)
+
+
+# pre: one line of the input
+def part2(values: list, res: int) -> bool:
+    nodes = [Node(values.pop(0), None)]
+    while values:
+        nodes.append(Node(values.pop(0), concat))
+
+    return run_recursive(nodes, res)
+
+
 with open("i7.txt", 'r') as inp:
+    part2sum = 0
     for line in inp:
         # initial parsing
         numbers = line.strip().split(' ')
@@ -58,9 +141,12 @@ with open("i7.txt", 'r') as inp:
 
         # prepare the numbers with tuples, start with plus everywhere
         result = numbers.pop(0)
-        first = [(numbers.pop(0), None)]  # no operator before first value
-        rest = [(i, False) for i in numbers]
+        # first = [(numbers.pop(0), None)]  # no operator before first value
+        # rest = [(i, False) for i in numbers]
 
-        run(first + rest, result)
+        # run(first + rest, result)
 
-    print(totsum)
+        if part2(numbers, result): part2sum += result
+
+    # print(totsum)
+    print(part2sum)
